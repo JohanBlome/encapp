1b104b803228d6e75ba061368bb2eae0986f870d
Johan Blome <jblome@fb.com>
2022-01-04 09:56:26 -0800

    encapp: simplification cli args
    Cleanup of the string parsing and bundle handling

--- app/src/androidTest/java/com/facebook/encapp/CodecValidationInstrumentedTest.java
+++ app/src/androidTest/java/com/facebook/encapp/CodecValidationInstrumentedTest.java
@@ -2,6 +2,7 @@ package com.facebook.encapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 
 import org.junit.Before;
@@ -55,58 +56,7 @@ public class CodecValidationInstrumentedTest {
 
     private static final String TARGET_PACKAGE =
             InstrumentationRegistry.getInstrumentation().getTargetContext().getPackageName();
-    private static final String MODE =
-            InstrumentationRegistry.getArguments().getString("mod");
-    private static final String ENCODER =
-            InstrumentationRegistry.getArguments().getString("enc");
-    private static final String BITRATE =
-            InstrumentationRegistry.getArguments().getString("bit");
-    private static final String REF_RESOLUTION =
-            InstrumentationRegistry.getArguments().getString("ref_res");
-    private static final String RESOLUTION =
-            InstrumentationRegistry.getArguments().getString("res");
-    private static final String KEYFRAME =
-            InstrumentationRegistry.getArguments().getString("key");
-    private static final String REF_FPS =
-            InstrumentationRegistry.getArguments().getString("ref_fps");
-    private static final String FPS =
-            InstrumentationRegistry.getArguments().getString("fps");
-    private static final String WRITE_FILE =
-            InstrumentationRegistry.getArguments().getString("write");
-    private static final String RUN =
-            InstrumentationRegistry.getArguments().getString("run");
-    private static final String FILE =
-            InstrumentationRegistry.getArguments().getString("file");
-    private static final String TEST_TIMEOUT =
-            InstrumentationRegistry.getArguments().getString("test_timeout");
-    private static final String VIDEO_TIMEOUT =
-            InstrumentationRegistry.getArguments().getString("video_timeout");
-    private static final String SKIPFRAMES =
-            InstrumentationRegistry.getArguments().getString("skfr");
-    private static final String DYNAMIC =
-            InstrumentationRegistry.getArguments().getString("dyn");
-    private static final String LTR_COUNT =
-            InstrumentationRegistry.getArguments().getString("ltrc");
-    private static final String LIST_CODECS =
-            InstrumentationRegistry.getArguments().getString("list_codecs");
-    private static final String IFRAME_SIZE_PRESET =
-            InstrumentationRegistry.getArguments().getString("ifsize");
-    private static final String TEMPORAL_LAYER_COUNT =
-            InstrumentationRegistry.getArguments().getString("tlc");
-    private static final String LOOP_INPUT =
-            InstrumentationRegistry.getArguments().getString("loop");
-    private static final String MULTIPLE_CONC_SESSIONS =
-            InstrumentationRegistry.getArguments().getString("conc");
-    private static final String TEST_CONFIG =
-            InstrumentationRegistry.getArguments().getString("test");
-    private static final String TEST_UI_HOLD_TIME_SEC =
-            InstrumentationRegistry.getArguments().getString("ui_hold_sec");
-    private static final String OLD_AUTH_METHOD=
-            InstrumentationRegistry.getArguments().getString("old_auth");
-    private static final String PURSUIT=
-            InstrumentationRegistry.getArguments().getString("pursuit");
-    private static final String REALTIME=
-            InstrumentationRegistry.getArguments().getString("realtime");
+
 
     private static long UI_TIMEOUT = 60 * 60 * 1000; //60 minutes in ms
 
@@ -114,125 +64,10 @@ public class CodecValidationInstrumentedTest {
     private MainActivity mMainActivity;
     private HashMap<String, String> mExtraDataHashMap;
 
-    /**
-     * Populates the hashmap with content.
-     */
-    public void collectExtraData() {
-        mExtraDataHashMap = new HashMap<String, String>();
-        if (MODE != null) {
-            mExtraDataHashMap.put("mod", MODE);
-            Log.e(TAG, "MODE: " + MODE);
-        }
-        if (ENCODER != null) {
-            mExtraDataHashMap.put("enc", ENCODER);
-            Log.e(TAG, "ENCODER: " + ENCODER);
-        }
-        if (BITRATE != null) {
-            mExtraDataHashMap.put("bit", BITRATE);
-            Log.e(TAG, "BITRATE: " + BITRATE);
-        }
-        if (RESOLUTION != null) {
-            mExtraDataHashMap.put("res", RESOLUTION);
-            Log.e(TAG, "RESOLUTION: " + RESOLUTION);
-        }
-        if (REF_RESOLUTION != null) {
-            mExtraDataHashMap.put("ref_res", REF_RESOLUTION);
-            Log.e(TAG, "REF_RESOLUTION: " + REF_RESOLUTION);
-        }
-        if (KEYFRAME != null) {
-            mExtraDataHashMap.put("key", KEYFRAME);
-            Log.e(TAG, "KEYFRAME: " + KEYFRAME);
-        }
-        if (FPS != null) {
-            mExtraDataHashMap.put("fps", FPS);
-            Log.e(TAG, "FPS: " + FPS);
-        }
-        if (REF_FPS != null) {
-            mExtraDataHashMap.put("ref_fps", FPS);
-            Log.e(TAG, "REF_FPS: " + REF_FPS);
-        }
-        if (WRITE_FILE != null) {
-            mExtraDataHashMap.put("write", WRITE_FILE);
-            Log.e(TAG, "WRITE_FILE: " + WRITE_FILE);
-        }
-        if (RUN != null) {
-            mExtraDataHashMap.put("run", RUN);
-            Log.e(TAG, "RUN: " + RUN);
-        }
-        if (VIDEO_TIMEOUT != null) {
-            mExtraDataHashMap.put("video_timeout", VIDEO_TIMEOUT);
-            Log.e(TAG, "VIDEO_TIMEOUT: " + VIDEO_TIMEOUT);
-        }
-        if (SKIPFRAMES != null) {
-            mExtraDataHashMap.put("skip_frames", SKIPFRAMES);
-            Log.e(TAG, "SKIP FRAMES: " + SKIPFRAMES);
-        }
-        if (DYNAMIC != null) {
-            mExtraDataHashMap.put("dyn", DYNAMIC);
-            Log.e(TAG, "DYNAMIC data: " + DYNAMIC);
-        }
-        if (LTR_COUNT != null) {
-            mExtraDataHashMap.put("ltrc", LTR_COUNT);
-            Log.e(TAG, "Ltr count: " + LTR_COUNT);
-        }
-        if (FILE != null) {
-            if (!FILE.contains("/")) {
-                Log.e("Path does not contain \"/\"", FILE);
-                System.exit(0);
-            }
-            mExtraDataHashMap.put("file", FILE);
-            Log.e(TAG, "FILE: " + FILE);
-        }
 
-        if (LIST_CODECS != null) {
-            mExtraDataHashMap.put("list_codecs", LIST_CODECS);
-            Log.e(TAG, "LIST_CODECS: " + LIST_CODECS);
-        }
-        if (IFRAME_SIZE_PRESET != null) {
-            mExtraDataHashMap.put("ifsize", IFRAME_SIZE_PRESET);
-            Log.e(TAG, "iframe size set: " + IFRAME_SIZE_PRESET);
-        }
-        if (TEMPORAL_LAYER_COUNT != null) {
-            mExtraDataHashMap.put("tlc", TEMPORAL_LAYER_COUNT);
-            Log.e(TAG, "Temporal layer count: " + TEMPORAL_LAYER_COUNT);
-        }
-        if (LOOP_INPUT != null) {
-            mExtraDataHashMap.put("loop", LOOP_INPUT);
-            Log.e(TAG, "loop input: " + LOOP_INPUT);
-        }
-        if (MULTIPLE_CONC_SESSIONS != null) {
-            mExtraDataHashMap.put("conc", MULTIPLE_CONC_SESSIONS);
-            Log.e(TAG, "concurrent sessions: " + MULTIPLE_CONC_SESSIONS);
-        }
-        if (TEST_CONFIG != null) {
-            mExtraDataHashMap.put("test", TEST_CONFIG);
-            Log.e(TAG, "test config: " + TEST_CONFIG);
-        }
-        if (TEST_UI_HOLD_TIME_SEC != null) {
-            mExtraDataHashMap.put("ui_hold_sec", TEST_UI_HOLD_TIME_SEC);
-            Log.e(TAG, "ui_hold_sec config: " + TEST_UI_HOLD_TIME_SEC);
-        }
-        if (OLD_AUTH_METHOD != null) {
-            mExtraDataHashMap.put("old_auth", OLD_AUTH_METHOD);
-            Log.e(TAG, "old_auth config: " + OLD_AUTH_METHOD);
-        }
-        if (PURSUIT != null) {
-            mExtraDataHashMap.put("pursuit", PURSUIT);
-            Log.e(TAG, "pursuit config: " + PURSUIT);
-        }
-        if (REALTIME != null) {
-            mExtraDataHashMap.put("realtime", REALTIME);
-            Log.e(TAG, "realtime config: " + REALTIME);
-        }
-    }
 
     @Before
     public void startMainActivityFromHomeScreen() {
-        if (TEST_TIMEOUT != null) {
-            UI_TIMEOUT = Integer.parseInt(TEST_TIMEOUT) * 60 * 1000;
-        }
-        Log.e(TAG, "UI_TIMEOUT: " + UI_TIMEOUT);
-
         // Initialize UiDevice instance
         mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
 
@@ -250,10 +85,8 @@ public class CodecValidationInstrumentedTest {
         final Intent intent = context.getPackageManager()
                 .getLaunchIntentForPackage(TARGET_PACKAGE);
 
-        // Add extra data to intent
-        Log.d(TAG, "Collect extra data");
-        collectExtraData();
-        intent.putExtra("map", mExtraDataHashMap);
+        Bundle bundle = InstrumentationRegistry.getArguments();
+        intent.putExtras(bundle);
         ActivityTestRule<MainActivity> mainActivityTestRule =
                 new ActivityTestRule<>(MainActivity.class);
         // Clear out any previous instances
---
+++ app/src/main/java/com/facebook/encapp/utils/ParseData.java
@@ -0,0 +1,30 @@
+package com.facebook.encapp.utils;
+
+public class ParseData {
+    public static final String MODE ="mod";
+    public static final String ENCODER ="enc";
+    public static final String BITRATE ="bit";
+    public static final String REF_RESOLUTION ="ref_res";
+    public static final String RESOLUTION ="res";
+    public static final String KEYFRAME ="key";
+    public static final String REF_FPS ="ref_fps";
+    public static final String FPS ="fps";
+    public static final String WRITE_FILE ="write";
+    public static final String RUN ="run";
+    public static final String FILE ="file";
+    public static final String TEST_TIMEOUT ="test_timeout";
+    public static final String VIDEO_TIMEOUT ="video_timeout";
+    public static final String SKIPFRAMES ="skfr";
+    public static final String DYNAMIC ="dyn";
+    public static final String LTR_COUNT ="ltrc";
+    public static final String LIST_CODECS ="list_codecs";
+    public static final String IFRAME_SIZE_PRESET ="ifsize";
+    public static final String TEMPORAL_LAYER_COUNT ="tlc";
+    public static final String LOOP_INPUT ="loop";
+    public static final String MULTIPLE_CONC_SESSIONS ="conc";
+    public static final String TEST_CONFIG ="test";
+    public static final String TEST_UI_HOLD_TIME_SEC ="ui_hold_sec";
+    public static final String OLD_AUTH_METHOD="old_auth";
+    public static final String PURSUIT="pursuit";
+    public static final String REALTIME="realtime";
+}
