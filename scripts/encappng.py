682df06866a850f5053519aa7c9a1069ab2451dc
Chema Gonzalez <chemag@fb.com>
2022-01-25 19:29:59 -0800

    encappng: rework main script
    Tested:
    
    ```
    $ ./scripts/encappng.py help
    usage: encappng.py [-h] [-d] [--quiet] [--serial SERIAL] [--install]
                       [--no-install]
                       [help: show help options | install: install apks | list: list codecs and devices supported | encode: run test cases]
                       [input-file] [output-dir]
    
    Python script to run ENCAPP tests on Android and collect results. The script
    will create a directory based on device model and date, and save encoded video
    and rate distortion results in the directory
    
    positional arguments:
      help: show help options | install: install apks | list: list codecs and devices supported | encode: run test case(s)
                            function arg
      input-file            input file
      output-dir            output dir
    
    options:
      -h, --help            show this help message and exit
      -d, --debug           Increase verbosity (use multiple times for more)
      --quiet               Zero verbosity
      --serial SERIAL       Android device serial number
      --install             Do install apk
      --no-install          Do not install apk
    ```

--- scripts/encappng.py
+++ scripts/encappng.py
@@ -27,6 +27,21 @@ MAIN_APK = os.path.join(
     SCRIPT_DIR,
     '/../app/build/outputs/apk/debug/com.facebook.encapp-v1.0-debug.apk')
 
+FUNC_CHOICES = {
+    'help': 'show help options',
+    'install': 'install apks',
+    'list': 'list codecs and devices supported',
+    'encode': 'run test case(s)',
+}
+
+default_values = {
+    'debug': 0,
+    'func': 'help',
+    'install': False,
+    'infile': None,
+    'outdir': None,
+}
+
 
 def run_cmd(cmd, silent=False):
     ret = True
@@ -259,9 +274,7 @@ def run_encode_tests(test_def, json_path, model, serial, test_desc,
     return result_json
 
 
-def list_codecs(serial, model, install):
-    if install:
-        install_app(serial)
+def list_codecs(serial, model):
 
     adb_cmd = f'adb -s {serial} shell am start '\
               f'-e ui_hold_sec 3 '\
@@ -279,30 +292,86 @@ def list_codecs(serial, model, install):
         print(f'File is available in current dir as {filename}')
 
 
+def convert_config(input_config, options):
+    test_config = ''
+    return test_config
+
+
+def encode_test(options, model, serial):
+    # get date and time and format it
+    now = datetime.now()
+    dt_string = now.strftime('%Y-%m-%d_%H_%M')
+
+    # get working directory directory
+    if options.output is not None:
+        workdir = options.output
+    else:
+        workdir = f'{options.desc.replace(" ", "_")}_{model}_{dt_string}'
+    os.system('mkdir -p ' + workdir)
+
+    # read input file
+    with open(options.infile, 'r') as fp:
+        if options.debug > 0:
+            print(f"infile: {options.infile} install: {options.install}")
+        input_config = json.load(fp)
+
+    # convert the human-friendly input into a valid apk input
+    test_config = convert_config(input_config, options)
+
+    # run the encode test
+    run_encode_tests(input_config,
+                     options.infile,
+                     model,
+                     serial,
+                     options.desc if options.desc is not None else '',
+                     workdir,
+                     options)
+
+
 def get_options(argv):
     parser = argparse.ArgumentParser(description=__doc__)
-
-    parser.add_argument('test', nargs='*', help='Test cases in JSON format.')
+    parser.add_argument(
+            '-d', '--debug', action='count',
+            dest='debug', default=default_values['debug'],
+            help='Increase verbosity (use multiple times for more)',)
+    parser.add_argument(
+            '--quiet', action='store_const',
+            dest='debug', const=-1,
+            help='Zero verbosity',)
     parser.add_argument('--serial', help='Android device serial number')
-    parser.add_argument('--config', help='Generate a sample config \
-                         file in json format')
-    parser.add_argument('-l', '--list_codecs', action='store_true',
-                        help='List codecs the devices support')
-    parser.add_argument('--desc', default='encapp', help='Test description')
-    parser.add_argument('-o', '--output', help='Name output directory')
-    parser.add_argument('--no_install', action='store_true',
-                        help='Do not install apk')
-
-    parser.add_argument('--codec', help='Override used codec', default='')
-    parser.add_argument('--input', help='Override input file', default='')
-    parser.add_argument('--input_res', help='Override input file', default='')
-    parser.add_argument('--input_fps', help='Override input fps', default='')
-    parser.add_argument('--output_fps', help='Override output fps', default='')
-    parser.add_argument('--output_res', help='Override output resolution',
-                        default='')
-    parser.add_argument('--no_split', action='store_true')
+    parser.add_argument(
+            '--install', action='store_const',
+            dest='install', const=True,
+            default=default_values['install'],
+            help='Do install apk',)
+    parser.add_argument(
+            '--no-install', action='store_const',
+            dest='install', const=False,
+            help='Do not install apk',)
+    parser.add_argument(
+            'func', type=str, nargs='?',
+            default=default_values['func'],
+            choices=FUNC_CHOICES.keys(),
+            metavar='%s' % (' | '.join("{}: {}".format(k, v) for k, v in
+                                       FUNC_CHOICES.items())),
+            help='function arg',)
+    parser.add_argument(
+            'infile', type=str, nargs='?',
+            default=default_values['infile'],
+            metavar='input-file',
+            help='input file',)
+    parser.add_argument(
+            'outdir', type=str, nargs='?',
+            default=default_values['outdir'],
+            metavar='output-dir',
+            help='output dir',)
 
     options = parser.parse_args(argv[1:])
+
+    # implement help
+    if options.func == 'help':
+        parser.print_help()
+        sys.exit(0)
 
     if options.serial is None and 'ANDROID_SERIAL' in os.environ:
         # read serial number from ANDROID_SERIAL env variable
@@ -313,38 +382,29 @@ def get_options(argv):
 
 def main(argv):
     options = get_options(argv)
-    if options.config is not None:
-        pass
-    else:
-        model, serial = get_device_info(options.serial)
-        if type(model) is dict:
-            if 'model' in model:
-                model = model.get('model')
-            else:
-                model = list(model.values())[0]
-        print(f'model = {model}')
-        if options.list_codecs is True:
-            list_codecs(serial, model, not options.no_install)
+
+    # get serial number
+    model, serial = get_device_info(options.serial, options.debug)
+    if type(model) is dict:
+        if 'model' in model:
+            model = model.get('model')
         else:
-            # get date and time and format it
-            now = datetime.now()
-            dt_string = now.strftime('%Y-%m-%d_%H_%M')
-            workdir = (
-                f"{options.desc.replace(' ', '_')}_{model}_{dt_string}")
-            if options.output is not None:
-                workdir = options.output
-            os.system('mkdir -p ' + workdir)
-            for test in options.test:
-                with open(test, 'r') as fp:
-                    tests_json = json.load(fp)
-                    run_encode_tests(tests_json,
-                                     test,
-                                     model,
-                                     serial,
-                                     options.desc if options.desc is
-                                     not None else '',
-                                     workdir,
-                                     options)
+            model = list(model.values())[0]
+    if options.debug > 0:
+        print(f'model = {model}')
+
+    # install app
+    if options.func == 'install' or options.install:
+        install_app(serial)
+
+    # run function
+    if options.func == 'list':
+        list_codecs(serial)
+
+    elif options.func == 'encode':
+        # ensure there is an input configuration
+        assert options.infile is not None, 'error: need a valid input file'
+        encode_test(options, model)
 
 
 if __name__ == '__main__':
