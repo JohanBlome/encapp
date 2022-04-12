4385626ecdf516622af041cfb9f503ad3b9a84c6
JohanBlome <johan@gunhildcarling.net>
2022-04-11 23:35:42 -0700

    encapp: update quality script
    Some changes in vmaf parsgin and fixes for situation with erronous
    size settings.

--- scripts/encapp_quality.py
+++ scripts/encapp_quality.py
@@ -16,7 +16,9 @@ from os.path import exists
 from encapp import run_cmd
 from encapp import convert_to_bps
 
-VMAF_RE = '(?<=aggregateVMAF=\")[0-9.]*'
+VMAF_RE = '<metric name=\"vmaf\" min=\"(?P<min>[0-9.]*)\" ' \
+          'max=\"(?P<max>[0-9.]*)\" mean=\"(?P<mean>[0-9.]*)\" ' \
+          'harmonic_mean=\"(?P<hmean>[0-9.]*)\" />'
 PSNR_RE = 'average:([0-9.]*)'
 SSIM_RE = 'SSIM Y:([0-9.]*)'
 FFMPEG_SILENT = 'ffmpeg  -hide_banner -loglevel error -y '
@@ -46,7 +48,7 @@ def parse_quality(vmaf_file, ssim_file, psnr_file):
             line = input_file.readline()
             match = re.search(VMAF_RE, line)
             if match:
-                vmaf = int(round(float(match.group(0))))
+                vmaf = int(round(float(match.group('mean'))))
                 break
     ssim = -1
     with open(ssim_file) as input_file:
@@ -70,6 +72,16 @@ def parse_quality(vmaf_file, ssim_file, psnr_file):
 
     return vmaf, ssim, psnr
 
+def get_media_props(mediapath):
+    status, std_out, std_err = run_cmd(f'ffprobe {mediapath}')
+    resolution = None
+    if status:
+        print(f'std_out = {std_out}, err = {std_err}')
+
+    m = re.search('([0-9]{2,4})x([0-9]{2,4})', std_err)
+    if m:
+        resolution = m.group(0)
+    return resolution
 
 def run_quality(test_file, optionals):
     """Compare the output found in test_file with the source/reference
@@ -115,21 +127,43 @@ def run_quality(test_file, optionals):
             if source.find('nv12') > -1:
                 pix_fmt = 'nv12'
 
-        if len(optionals['override_reference']) > 0:
-            input_res = optionals['reference_resolution']
-        else:
-            input_width = input_media_format.get('width')
-            input_height = input_media_format.get('height')
-            input_res = f'{input_width}x{input_height}'
         output_media_format = test.get('encoder_media_format')
         output_width = output_media_format.get('width')
         output_height = output_media_format.get('height')
 
         output_res = f'{output_width}x{output_height}'
+        media_res = get_media_props(encodedfile)
+        if output_res != media_res:
+            print(f'Warning. Discrepancy in resolutions for output')
+            print(f'Json {output_res}, media {media_res}')
+            output_res = media_res
+
+        if len(optionals['reference_resolution']) > 0:
+            input_res = optionals['reference_resolution']
+        else:
+
+            input_width = input_media_format.get('width')
+            input_height = input_media_format.get('height')
+            #If we did not get aything here use the encoded size
+            if input_width == None or input_height is None or \
+                (not input_width.isnumeric() and not input_height.isnumeric()):
+                print(f'Warning. Input size if wrong.')
+                print(f'Json {input_width}x{input_height}')
+                input_res = output_res
+            else:
+                input_res = f'{input_width}x{input_height}'
+
 
         reference = source
+        if not os.path.exists(reference):
+            print(f'Reference {reference} is unavailable')
+            exit(-1)
         distorted = encodedfile
-        adb_cmd = ''
+        if not os.path.exists(source):
+            print(f'Source {source} is unavailable')
+            exit(-1)
+
+        shell_cmd = ''
 
         force_scale = ''
         if optionals['fr_fr']:
@@ -145,16 +179,17 @@ def run_quality(test_file, optionals):
             distorted = f'{encodedfile}.yuv'
 
             # Scale
-            adb_cmd = (f'{FFMPEG_SILENT} -i {encodedfile} -f rawvideo '
+            shell_cmd = (f'{FFMPEG_SILENT} -i {encodedfile} -f rawvideo '
                        f'-pix_fmt {pix_fmt} -s {input_res} {distorted}')
 
-            run_cmd(adb_cmd)
+            run_cmd(shell_cmd)
         if raw:
             ref_part = (f'-f rawvideo -pix_fmt {pix_fmt} -s {input_res} '
                         f'-r {fps} -i {reference} ')
         else:
             ref_part = f'-r {fps} -i {reference} '
 
+        print(f'input res = {input_res} vs {output_res}')
         if input_res != output_res:
             dist_part = (f'-f rawvideo -pix_fmt {pix_fmt} '
                          f'-s {input_res} -r {fps} -i {distorted}')
@@ -163,29 +198,29 @@ def run_quality(test_file, optionals):
 
         # Do calculations
         if optionals['recalc'] or not exists(vmaf_file):
-            adb_cmd = (f'{FFMPEG_SILENT} {ref_part} {dist_part} '
+            shell_cmd = (f'{FFMPEG_SILENT} {ref_part} {dist_part} '
                        '-filter_complex '
-                       f'"{force_scale}libvmaf=log_path={vmaf_file}" '
+                       f'"{force_scale}libvmaf=log_path={vmaf_file}:n_threads=16" '
                        '-report -f null - 2>&1 ')
-            run_cmd(adb_cmd)
+            run_cmd(shell_cmd)
         else:
             print(f'vmaf already calculated for media, {vmaf_file}')
 
         if optionals['recalc'] or not exists(ssim_file):
-            adb_cmd = (f'ffmpeg {dist_part} {ref_part} '
+            shell_cmd = (f'ffmpeg {dist_part} {ref_part} '
                        '-filter_complex '
                        f'"{force_scale}ssim=stats_file={ssim_file}.all" '
                        f'-f null - 2>&1 | grep SSIM > {ssim_file}')
-            run_cmd(adb_cmd)
+            run_cmd(shell_cmd)
         else:
             print(f'ssim already calculated for media, {ssim_file}')
 
         if optionals['recalc'] or not exists(psnr_file):
-            adb_cmd = (f'ffmpeg {dist_part} {ref_part} '
+            shell_cmd = (f'ffmpeg {dist_part} {ref_part} '
                        '-filter_complex '
                        f'"{force_scale}psnr=stats_file={psnr_file}.all" '
                        f'-f null - 2>&1 | grep PSNR > {psnr_file}')
-            run_cmd(adb_cmd)
+            run_cmd(shell_cmd)
         else:
             print(f'psnr already calculated for media, {psnr_file}')
 
@@ -287,7 +322,8 @@ def main(argv):
 
         for test in options.test:
             data = run_quality(test, settings)
-            output.write(data)
+            if data != None:
+                output.write(data)
 
 
 if __name__ == '__main__':
