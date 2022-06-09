e95dcfb1eb55c3b6ca20de3287364780279539c2
Hideki Mendoza <hidekim@fb.com>
2022-06-09 11:46:41 -0700

    encapp: add system test for encapp.py script
    Add system test for encapp.py script intending to use real
    android device connected to the host machine.
    Split scripts/tests directory into system and unit tests
    Unit tests intended to be run with pytest or unittest
    System tests intended to be run with pytest
    $ ANDROID_SERIAL=<serial_no> python3 -m pytest tests/system/test_encapp.py

---
+++ scripts/tests/system/test_encapp.py
@@ -0,0 +1,39 @@
+import os
+import subprocess
+
+PYTHON_ENV = "python3"
+MODULE_PATH = os.path.dirname(__file__)
+ENCAPP_SCRIPTS_DIR = os.path.join(MODULE_PATH, os.pardir, os.pardir)
+ENCAPP_SCRIPT_PATH = os.path.join(ENCAPP_SCRIPTS_DIR, "encapp.py")
+ANDROID_SERIAL = os.getenv("ANDROID_SERIAL")
+
+assert ANDROID_SERIAL is not None, "ANDROID_SERIAL environment variable must be defined"
+
+
+def test_is_encapp_script_found():
+    """Verify encapp.py script is at expected path"""
+    assert os.path.isfile(ENCAPP_SCRIPT_PATH)
+
+
+def test_help_option():
+    """Verify encapp.py --help do not throw any error"""
+    subprocess.run(
+        [f"{PYTHON_ENV} {ENCAPP_SCRIPT_PATH} "
+         f"--help"],
+        shell=True,
+        check=True,
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+    )
+
+
+def test_install():
+    """Verify installation work on specified android device"""
+    subprocess.run(
+        [f"{PYTHON_ENV} {ENCAPP_SCRIPT_PATH} "
+         f"--serial {ANDROID_SERIAL} install"],
+        shell=True,
+        check=True,
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+    )
