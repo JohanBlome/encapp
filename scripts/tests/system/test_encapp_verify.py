f33da05de6e4713a1beac0c84beac9726a201215
Hideki Mendoza <hidekim@fb.com>
2022-06-27 10:46:43 -0700

    encapp: add system test for verify
    Add system test for encapp_verify.py -h

---
+++ scripts/tests/system/test_encapp_verify.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+
+PYTHON_ENV = "python3"
+MODULE_PATH = os.path.dirname(__file__)
+ENCAPP_SCRIPTS_DIR = os.path.join(MODULE_PATH, os.pardir, os.pardir)
+VERIFY_SCRIPT_PATH = os.path.join(ENCAPP_SCRIPTS_DIR, "encapp_verify.py")
+ANDROID_SERIAL = os.getenv("ANDROID_SERIAL")
+
+assert ANDROID_SERIAL is not None, "ANDROID_SERIAL environment variable must be defined"
+
+
+def test_is_verify_script_found():
+    """Verify encapp_verify.py script is at expected path"""
+    assert os.path.isfile(VERIFY_SCRIPT_PATH)
+
+
+def test_help_option():
+    """Verify encapp_verify.py --help do not throw any error"""
+    subprocess.run(
+        [f"{PYTHON_ENV} {VERIFY_SCRIPT_PATH} "
+         f"--help"],
+        shell=True,
+        check=True,
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
