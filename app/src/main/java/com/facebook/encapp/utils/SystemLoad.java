b2edb8972176bf025fddf35fea365a0b7a7c34eb
Chema Gonzalez <chemag@fb.com>
2022-09-30 08:52:33 -0700

    encapp: remove error message when GPU info does not exist
    The exact paths of the GPU data reading code only work for some QCOM
    SoCs. Also, the error message in that case was incorrect.
    
    If the directory does not exist, let's just add a warning.
    
    Before:
    ```
    ...
    09-16 13:28:25.712  7546  7572 E encapp.systemload: Failed to read: /sys/class/kgsl/kgsl-3d0/gpu_model
    09-16 13:28:25.712  7546  7572 E encapp.systemload: Could not read system data, "adb root && adb shell setenforce 0" needed
    ...
    ```
    
    After:
    ```
    ...
    09-12 19:39:05.921  3384  3419 W encapp.systemload: QCOM GPU info dir does not exist: /sys/class/kgsl/kgsl-3d0/
    ...
    ```

--- app/src/main/java/com/facebook/encapp/utils/SystemLoad.java
+++ app/src/main/java/com/facebook/encapp/utils/SystemLoad.java
@@ -7,7 +7,9 @@ import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -17,6 +19,14 @@ import java.util.stream.Stream;
 
 public class SystemLoad {
     protected static final String TAG = "encapp.systemload";
+    protected static final String FILE_QCOM_GPU_INFO_DIR = "/sys/class/kgsl/kgsl-3d0/";
+    protected static final String FILE_QCOM_GPU_MODEL = FILE_QCOM_GPU_INFO_DIR + "gpu_model";
+    protected static final String FILE_QCOM_GPU_MIN_CLOCK = FILE_QCOM_GPU_INFO_DIR + "min_clock_mhz";
+    protected static final String FILE_QCOM_GPU_MAX_CLOCK = FILE_QCOM_GPU_INFO_DIR + "max_clock_mhz";
+    protected static final String FILE_QCOM_GPU_BUSY_PERCENTAGE = FILE_QCOM_GPU_INFO_DIR + "gpu_busy_percentage";
+    protected static final String FILE_QCOM_GPU_CLOCK_MHZ = FILE_QCOM_GPU_INFO_DIR + "clock_mhz";
+
+
     protected boolean mCannotRead = false;
     Boolean started = Boolean.FALSE;
     int mFrequencyHz = 10;
@@ -24,7 +34,13 @@ public class SystemLoad {
     ArrayList<String> mGpuLoad = new ArrayList<>();
     ArrayList<String> mGpuClock = new ArrayList<>();
     public void start() {
-        String tmp = readSystemData("/sys/class/kgsl/kgsl-3d0/gpu_model");
+        // check whether the directory exists
+        if (! Files.exists(Paths.get(FILE_QCOM_GPU_INFO_DIR))) {
+            Log.w(TAG, "QCOM GPU info dir does not exist: " + FILE_QCOM_GPU_INFO_DIR);
+            return;
+        }
+
+        String tmp = readSystemData(FILE_QCOM_GPU_MODEL);
         if (tmp.equals("")) {
             Log.e(TAG, "Could not read system data, \"adb root && adb shell setenforce 0\" needed");
             return;
@@ -32,9 +48,9 @@ public class SystemLoad {
             Log.e(TAG, "GPU model: "+tmp);
         }
         mGPUInfo.put("gpu_model", tmp.trim());
-        tmp = readSystemData("/sys/class/kgsl/kgsl-3d0/min_clock_mhz");
+        tmp = readSystemData(FILE_QCOM_GPU_MIN_CLOCK);
         mGPUInfo.put("gpu_min_clock", tmp.trim());
-        tmp = readSystemData("/sys/class/kgsl/kgsl-3d0/max_clock_mhz");
+        tmp = readSystemData(FILE_QCOM_GPU_MAX_CLOCK);
         mGPUInfo.put("gpu_max_clock", tmp.trim());
 
         if (mCannotRead) {
@@ -53,9 +69,9 @@ public class SystemLoad {
                             //Read cpu
                             //tmp = readSystemData("/proc/stat");
                             //gpu
-                            tmp = readSystemData("/sys/class/kgsl/kgsl-3d0/gpu_busy_percentage");
+                            tmp = readSystemData(FILE_QCOM_GPU_BUSY_PERCENTAGE);
                             mGpuLoad.add(tmp.trim());
-                            tmp = readSystemData("/sys/class/kgsl/kgsl-3d0/clock_mhz");
+                            tmp = readSystemData(FILE_QCOM_GPU_CLOCK_MHZ);
                             mGpuClock.add(tmp.trim());
                         }
 
