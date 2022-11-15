78b4fe13df0e8fcb4aaf171822923539868ab179
JohanBlome <jblome@fb.com>
2022-11-15 15:20:54 -0800

    encapp: Set zero b-frames when testing
    Signed-off-by: JohanBlome <jblome@fb.com>

--- scripts/prepare_test_data.sh
+++ scripts/prepare_test_data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 curl  https://media.xiph.org/video/derf/y4m/akiyo_qcif.y4m -o /tmp/akiyo_qcif.y4m
-ffmpeg -y -i /tmp/akiyo_qcif.y4m  /tmp/akiyo_qcif.mp4
+ffmpeg -y -i /tmp/akiyo_qcif.y4m -c:v libx264 -bf 0  /tmp/akiyo_qcif.mp4
 ffmpeg -y -i /tmp/akiyo_qcif.y4m -f rawvideo -pix_fmt nv21  /tmp/akiyo_qcif_nv21.yuv
 ffmpeg -y -i /tmp/akiyo_qcif.y4m -f rawvideo -pix_fmt yuv420p  /tmp/akiyo_qcif.yuv
