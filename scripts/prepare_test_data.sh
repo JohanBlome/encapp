edd3a8262388634573d8e9fac1f05015c3295492
Chema Gonzalez <chemag@gmail.com>
2023-01-18 14:38:53 -0800

    encapp: scripts: produce yuv version of akiyo
    This should make the README simpler.

--- scripts/prepare_test_data.sh
+++ scripts/prepare_test_data.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 curl https://media.xiph.org/video/derf/y4m/akiyo_qcif.y4m -o /tmp/akiyo_qcif.y4m
+ffmpeg -i /tmp/akiyo_qcif.y4m -f rawvideo -pix_fmt yuv420p /tmp/akiyo_qcif.yuv
 ffmpeg -y -i /tmp/akiyo_qcif.y4m -c:v libx264 -bf 0 /tmp/akiyo_qcif.mp4
