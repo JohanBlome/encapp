d67c2eb4a7ea358734be12cb207c8f5bbfe202ce
JohanBlome <jblome@fb.com>
2025-04-04 23:05:16 -0700

    encapp: new pbtxt merge test example added The tests/merge_protobuf.sh script will run two encodings and compare the colors of the encoded file.
    Signed-off-by: JohanBlome <jblome@fb.com>

---
+++ tests/merge_protobuf.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+printf "** Testing merge of pbtxt files **\n"
+cdir=$(dirname "$0")
+python3 ${cdir}/../scripts/encapp.py run ${cdir}/surface_transcoder.pbtxt ${cdir}/output_name.pbtxt -w /tmp/colortest.original
+printf "\nAdding color1.pbtxt containing updated settings for color encoding\n"
+python3 ${cdir}/../scripts/encapp.py run ${cdir}/surface_transcoder.pbtxt ${cdir}/output_name.pbtxt ${cdir}/color1.pbtxt -w /tmp/colortest.color1
+
+printf "\n\n * Original color information *\n"
+ffprobe /tmp/colortest.original/akiyo_qcif.default.transcode.mp4 2>&1 | grep -iEo "yuv420p\([0-9a-zA-Z, -\/]*\)"
+printf "\n * Updated color information *\n"
+ffprobe /tmp/colortest.color1/akiyo_qcif.default.transcode.mp4 2>&1 | grep -iEo "yuvj420p\([0-9a-zA-Z, -\/]*\)"
+
